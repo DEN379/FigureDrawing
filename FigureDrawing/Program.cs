5715e0bda6e196e80d13bc14cf3b1533fdb83a55
Den <denyssakadel@gmail.com>
2021-01-17 14:00:29 +0200

    init

---
+++ FigureDrawing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FigureDrawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Figure drawing, made by Denys Sakadel");
+        }
+    }
+}
